ccb42899b42e26e7331885b586839ed636d6d83f
Stariel Isaac <starielisaac@gmail.com>
2023-04-25 21:32:17 -0300

    feature: created middlewares 'validateName/validadeAmount

--- src/routers/productsRouter.ts
+++ src/routers/productsRouter.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 import productsController from '../controllers/productsController';
+import valitateName from '../middleware/valitateName';
+import valitateAmount from '../middleware/validateAmount';
 
 const productsRouter = Router();
 
 productsRouter.get('', productsController.findAllProducts);
-productsRouter.post('', productsController.registerProduct);
+productsRouter.post('', valitateName, valitateAmount, productsController.registerProduct);
 
 export default productsRouter;
